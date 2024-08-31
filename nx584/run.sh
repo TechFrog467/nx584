48b681055471fc573035dab2356639409c83229c
TechFrog467 <51302711+TechFrog467@users.noreply.github.com>
2024-08-31 15:05:24 -0400

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --serial /dev/ttyS0 --baud 9600 --config /usr/app/src/config.ini
+python3 /usr/bin/nx584_server --listen 192.168.200.240 --port 5007 --serial /dev/ttyS0 --baud 9600 --config /usr/app/src/config.ini
